f3c49282e6e1ab30d35b237a5a2bbad23650518c
Akash Roy <akashroy65123@gmail.com>
2025-10-09 23:37:06 +0600

    addednew files

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from '../pages/Root/Root';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hellobdagzrssssssssssss World</div>,
+    // element: <div>Hellobdagzrssssssssssss World</div>,
+    Component: Root,
+    errorElement:<ErrorPage></ErrorPage>
   },
 ]);
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            <h1>hello ami navbaqr</h1>
+            <h1>hello ami navbar</h1>
         </div>
     );
 };
